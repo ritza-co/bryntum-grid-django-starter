5467406e1bee835561a9381766091c9438743ef9
Tatenda <31291528+rideam@users.noreply.github.com>
2024-06-19 16:17:19 +0200

    initial app

--- grid/wsgi.py
+++ grid/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bryntumdemo project.
+WSGI config for grid project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bryntumdemo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grid.settings')
 
 application = get_wsgi_application()
